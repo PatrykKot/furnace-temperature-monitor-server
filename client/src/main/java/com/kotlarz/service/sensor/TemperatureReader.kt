286f1cdb9f021e8083016640b5454710913e84ec
Patryk Kotlarz <pkotlarz@suncode.pl>
2019-01-10 10:57:40 +0100

    Split to separate projects

--- client/src/main/java/com/kotlarz/service/sensor/TemperatureReader.kt
+++
@@ -1,9 +0,0 @@
-package com.kotlarz.service.sensor
-
-import com.kotlarz.service.dto.TemperatureLog
-
-interface TemperatureReader {
-    fun readAll(): List<TemperatureLog>
-
-    fun refreshDevices()
-}
---
+++ src/main/java/com/kotlarz/backend/web/dto/NewTemperatureDto.java
@@ -0,0 +1,15 @@
+package com.kotlarz.backend.web.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class NewTemperatureDto
+{
+    private Date date;
+
+    private String address;
+
+    private Double value;
+}
