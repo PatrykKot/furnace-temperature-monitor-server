b25c5a19d2ae0a5c031d1945460380301249dea7
Patryk Kotlarz <pkotlarz@suncode.pl>
2019-01-10 11:27:12 +0100

    Project refactor

--- src/main/java/com/kotlarz/Application.java
+++ src/main/java/com/kotlarz/Application.java
@@ -1,13 +1,11 @@
-package com.kotlarz.application;
+package com.kotlarz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@ComponentScan( "com.kotlarz" )
 public class Application
 {
     public static void main( String[] args )
