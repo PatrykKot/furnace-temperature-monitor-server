9ac3124ce779370dcf03cc99de6efdec10146fad
Patryk Kotlarz <pkotlarz@suncode.pl>
2019-01-13 11:47:07 +0100

    CORS fix

--- src/main/java/com/kotlarz/configuration/RestConfiguration.java
+++ src/main/java/com/kotlarz/configuration/RestConfiguration.java
@@ -23,6 +23,7 @@ public class RestConfiguration
     @Override
     public void addCorsMappings( CorsRegistry registry )
     {
-        registry.addMapping( "*" ).allowedOrigins( "*" );
+        registry.addMapping( "/**" )
+                        .allowedOrigins( "*" );
     }
 }
